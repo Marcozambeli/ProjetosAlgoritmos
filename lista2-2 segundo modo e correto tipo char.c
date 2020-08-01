52800660921e2d389a3cfb4160193746ef260801
Marco Antonio de O. Zambeli <68858899+Marcozambeli@users.noreply.github.com>
2020-08-01 16:07:50 -0300

    Add files via upload

---
+++ lista2-2 segundo modo e correto tipo char.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  lista 2-2segundo modo e correto por tipo char
+//
+//  Created by Marco Antonio Zambeli on 07/07/13.
+//  Copyright (c) 2013 Marco Antonio Zambeli. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char conceito (int media)
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
